764e1b6b3f52de1a9c2da936e2fee2b278097b93
Mandar <kp30023002@gmail.com>
2026-03-04 01:09:01 +0530

    AI-dought solver .env file fix

--- server.js
+++ server.js
@@ -673,7 +673,7 @@ app.post('/api/ai-doubt', async (req, res) => {
         // STEP 1: Ask Gemini to classify
         // ===============================
         const classificationResponse = await axios.post(
-            `https://generativelanguage.googleapis.com/v1beta/models/gemini-2.5-flash:generateContent?key=AIzaSyDFPcvxFxdcEpdxZ4q_-Rlb8nQ3cgeU-3k`,
+            `https://generativelanguage.googleapis.com/v1beta/models/gemini-2.5-flash:generateContent?key=${process.env.GEMINI_API_KEY}`,
             {
                 contents: [{
                     parts: [{
